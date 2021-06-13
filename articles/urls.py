a22992f0be9485518010e279a3d9c6927ec14624
Johnny Jimenez <johnny.robert.jimenez@gmail.com>
2021-06-13 12:02:56 -0700

    first commit

---
+++ articles/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import ArticleDeleteView, ArticleDetailView, ArticleListView, ArticleUpdateView, ArticleCreateView
+
+# 127.0.0.1:8000/articles/[url paths from below]
+# articles/id/edit/ --> users can modify
+# articles/id/delete/ --> users can remove article
+# articles/id/edit/ --> users can modify
+
+urlpatterns = [
+    path('', ArticleListView.as_view(), name= 'article_list'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('new', ArticleCreateView.as_view(), name='article_new'),
+]
---
+++ articles/views.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
+from .models import Article
+
+class ArticleListView(ListView):
+    template_name = 'article_list.html'
+    model = Article
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Article
+    template_name = 'article_edit.html'
+    fields = ('title', 'body', 'category')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    template_name = 'article_create.html'
+    fields = ('title', 'body','category')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
